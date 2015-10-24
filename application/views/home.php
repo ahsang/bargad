48c71f03666eda6b1bf93fe7b505d61f45b59fdb
Ahsan Gondal <ahsangondal15@gmail.com>
2015-10-24 16:42:55 +0500

    hello

--- application/views/home.php
+++ application/views/home.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="<?php echo includes_dir;?>common-files/images/favicon.png" type="image/png">
-    <link rel="stylesheet" href="<?php echo includes_dir;?>common-files/css/style.css"/>
+    <link rel="stylesheet" href="<?php echo includes_dir;?>common-files/css/style.min.css"/>
     <link rel="stylesheet" href="<?php echo includes_dir;?>common-files/css/custom.css"/>
     <link rel="stylesheet" id="scheme-source" href="<?php echo includes_dir;?>common-files/css/schemes/dark.css" />
     <!-- [if lt IE 9]>
@@ -25,12 +25,21 @@
         // instantiate the accordion
         $('#example1').accordionSlider({
           width: "100%",
-          height: "70%",
+          height: "80%",
           responsiveMode: 'auto',
           visiblePanels: 4,
           closePanelsOnMouseOut: true,
-          autoplay: false
+          autoplay: false,
+          breakpoints: {
+              960: {visiblePanels: 4},
+              800: {visiblePanels: 4, orientation: 'vertical', aspectRatio: 0.1},
+              650: {visiblePanels: 4},
+              500: {visiblePanels: 4, orientation: 'vertical', aspectRatio: 0.1}
+          }
         });
+        var accordion = $('#example1').data('accordionSlider');
+        $("#slider").height(accordion.$panelsContainer[0].clientHeight);
+        console.log(accordion.$panelsContainer[0].clientHeight);
 
         // change the responsive mode
         
@@ -362,7 +371,7 @@
     <!-- Begin Top Image -->
     <header class="center header-sm bg-dark parallax" data-img="bg/smoke2.png">
       <div class="container">
-          <h1 class="header-title center">BARGAD ORGANIZATION FOR YOUTH DEVELOPMENT</h1>
+          <!-- <h4 class="header-title center"><br></h4> -->
       </div>
         
       
@@ -372,104 +381,80 @@
         
         <!-- <div class="col-md-12" > -->
     <div class="full-width bg-dark" id="slider">
-      <div id="example1" class="" style="margin-bottom:10px;z-index:100">
+      <div id="example1" class="" style="padding-bottom:10px;z-index:100">
           <div class="as-panels" >
 
             <!-- Panel 1 -->
             <div class="as-panel" >
               <a href="http://bqworks.com">
-                <img class="as-background" src="<?echo includes_dir;?>common-files/dist/css/images/blank.gif" data-src="<?echo includes_dir;?>common-files/images/home/bargad1.png" data-retina="http://bqworks.com/accordion-slider/images/image1@2x.jpg"/>
+                <img class="as-background"  data-src="<?echo includes_dir;?>common-files/images/home/peace.png" />
               </a>
 
-              <div class="as-layer as-closed as-white panel-counter" 
-                 data-position="bottomLeft" data-horizontal="8" data-vertical="8">
-                 Panel 1
+              <div class="as-layer as-opened  "
+                 data-horizontal="center" data-vertical="70%" data-width="80%">
+                 <img src="<?echo includes_dir;?>common-files/images/home/text_peace.png" style="width:100%;"/>
+              </div>
+              <div class="as-layer as-closed  "
+                 data-horizontal="bottomLeft" data-vertical="80%" data-horizontal="5%" style="width:100%;">
+                 <h3 class="">Peace and Youth <br>Co operation</h3>
               </div>
 
-              <h3 class="as-layer as-opened as-black as-padding" 
-                data-horizontal="40" data-vertical="10%" 
-                data-show-transition="left" data-hide-transition="left">
-                Lorem ipsum dolor sit amet
-              </h3>
-              <p class="as-layer as-opened as-white as-padding hide-medium-screen" 
-                data-horizontal="40" data-vertical="22%" 
-                data-show-transition="left" data-show-delay="200" data-hide-transition="left" data-hide-delay="200">
-                consectetur adipisicing elit
-              </p>
-              <p class="as-layer as-opened as-black as-padding hide-small-screen" 
-                data-horizontal="40" data-vertical="34%" data-width="350" 
-                data-show-transition="left" data-show-delay="400" data-hide-transition="left" data-hide-delay="500">
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+              <p class="as-layer as-opened as-black as-padding" 
+                data-position="bottomLeft"
+                data-show-transition="up" data-hide-transition="down">
+                Lorem ipsum dolor sit amet <span class="hide-small-screen">, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> <span class="hide-medium-screen">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
               </p>
             </div>
 
                 <!-- Panel 2 -->
                 <div class="as-panel">
               <a href="http://bqworks.com">
-                <img class="as-background" src="<?echo includes_dir;?>common-files/dist/css/images/blank.gif" data-src="<?echo includes_dir;?>common-files/images/home/bargad2.png" data-retina="http://bqworks.com/accordion-slider/images/image2@2x.jpg"/>
+                <img class="as-background"  data-src="<?echo includes_dir;?>common-files/images/home/women.png" />
               </a>
 
               <div class="as-layer as-closed  "
-                 data-horizontal="center" data-vertical="30%">
-                 <h3 class="center">Peace and Youth <br>Co operation</h3>
+                 data-horizontal="bottomLeft" data-vertical="80%" data-horizontal="5%" style="width:100%;">
+                 <h3 class="">Women <br>Empowerment</h3>
               </div>
 
-              <div class="as-layer" 
-                data-position="bottomLeft" data-horizontal="20" data-vertical="20" data-width="100%">
-                
-                <p class="as-layer as-opened as-white as-padding"
-                  data-position="bottomLeft"
-                  data-show-transition="left" data-hide-transition="up" data-show-delay="400" data-hide-delay="200">
-                  Lorem ipsum
-                </p>
-
-                <p class="as-layer as-opened as-black as-padding hide-small-screen"
-                  data-position="bottomLeft" data-horizontal="120"
-                  data-show-transition="left" data-hide-transition="up" data-show-delay="600" data-hide-delay="100">
-                  dolor sit amet
-                </p>
-
-                <p class="as-layer as-opened as-white as-padding hide-small-screen"
-                  data-position="bottomLeft" data-horizontal="245"
-                  data-show-transition="left" data-hide-transition="up" data-show-delay="800">
-                  consectetur adipisicing elit.
-                </p>
-              </div>
+              <p class="as-layer as-opened as-black as-padding" 
+                data-position="bottomLeft"
+                data-show-transition="up" data-hide-transition="down">
+                Lorem ipsum dolor sit amet <span class="hide-small-screen">, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> <span class="hide-medium-screen">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+              </p>
             </div>
 
                 <!-- Panel 3 -->
                 <div class="as-panel">
               <a href="http://bqworks.com">
-                <img class="as-background" src="<?echo includes_dir;?>common-files/dist/css/images/blank.gif" data-src="http://bqworks.com/accordion-slider/images/image3.jpg" data-retina="http://bqworks.com/accordion-slider/images/image3@2x.jpg"/>
+                <img class="as-background"  data-src="<?echo includes_dir;?>common-files/images/home/youth.png" />
               </a>
 
-              <div class="as-layer as-closed as-white panel-counter"
-                 data-position="bottomLeft" data-horizontal="8" data-vertical="8">
-                 Panel 3
+              <div class="as-layer as-closed  "
+                 data-horizontal="bottomLeft" data-vertical="80%" data-horizontal="5%" style="width:100%;">
+                 <h3 class="">Youth Participation <br>and Engagement</h3>
               </div>
 
-              <p class="as-layer as-opened as-white as-padding" 
-                data-horizontal="center" data-vertical="44%" 
-                data-show-transition="right" data-hide-transition="left" data-show-delay="500" >
-                Lorem ipsum dolor sit amet
-              </p>
-              <p class="as-layer as-opened as-black as-padding hide-small-screen" 
-                data-horizontal="center" data-vertical="58%" 
-                data-show-transition="left" data-show-delay="700" data-hide-transition="right" data-hide-delay="200">
-                consectetur adipisicing elit
+
+              <p class="as-layer as-opened as-black as-padding" 
+                data-position="bottomLeft"
+                data-show-transition="up" data-hide-transition="down">
+                Lorem ipsum dolor sit amet <span class="hide-small-screen">, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span> <span class="hide-medium-screen">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
               </p>
             </div>
 
                 <!-- Panel 4 -->
                 <div class="as-panel">
               <a href="http://bqworks.com">
-                <img class="as-background" src="<?echo includes_dir;?>common-files/dist/css/images/blank.gif" data-src="http://bqworks.com/accordion-slider/images/image4.jpg" data-retina="http://bqworks.com/accordion-slider/images/image4@2x.jpg"/>
+                <img class="as-background"  data-src="<?echo includes_dir;?>common-files/images/home/policy.png" />
               </a>
 
-              <div class="as-layer as-closed as-white panel-counter"
-                 data-position="bottomLeft" data-horizontal="8" data-vertical="8">
-                 Panel 4
+              <div class="as-layer as-closed  "
+                 data-horizontal="bottomLeft" data-vertical="80%" data-horizontal="5%" style="width:100%;">
+                 <h3 class="">Youth Policy <br>and Advocacy</h3>
               </div>
+
 
               <p class="as-layer as-opened as-black as-padding" 
                 data-position="bottomLeft"
@@ -482,109 +467,39 @@
                 
           </div>
       </div>
-      
-      <!-- <div class="row"> -->
-      <br>
-            <div class="col-md-3">
-              <h4>Company Info</h4>
-              <p>Web agency created in 2014</p>
-              <p>Specialized in theme design template</p>
-              <p>Contact us for custom project</p>
-              <p>Make your own template with us</p>
-            </div>
-            <div class="col-md-3">
-              <h4>Contact Info</h4>
-              <p><i class="line-icon-map"></i>44 Main Street, New York, NY 25442</p>
-              <p><i class="line-icon-screen-smartphone"></i>+2-777-555-332 / -2-666-442-887</p>
-              <p><i class="line-icon-envelope-open"></i>support@themes-lab.com</p>
-              <p><i class="line-icon-calendar"></i>9am - 6 pm monday / saturday</p>
-            </div>
-            <div class="col-md-3">
-              <h4>Support</h4>
-              <p><a href="#">Faq</a></p>
-              <p><a href="#">Terms of Services</a></p>
-              <p><a href="#">Privacy Policy</a></p>
-              <p><a href="#">Contact us</a></p>
-            </div>
-            <div class="col-md-3">
-              <h4>Get In Touch</h4>
-              <div class="social-icons social-square">
-                <ul class="text-left">
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="300"><i class="fa fa-facebook"></i></a></li>
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="500"><i class="fa fa-twitter"></i></a></li>
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="700"><i class="fa fa-google-plus"></i></a></li>
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="900"><i class="fa fa-pinterest"></i></a></li>
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="1000"><i class="fa fa-flickr"></i></a></li>
-                  <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="1200"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
-              </div>
-            </div>
-        <!-- </div> -->
-
     </div>
-
-    <!-- Begin Footer 3 columns Dark -->
-    <div class="section-footer footer-wrap bg-dark">
-      <div class="container footer center">
-        <div class="row">
-          <div class="col-md-3">
-            <h4>Company Info</h4>
-            <p>Web agency created in 2014</p>
-            <p>Specialized in theme design template</p>
-            <p>Contact us for custom project</p>
-            <p>Make your own template with us</p>
-          </div>
-          <div class="col-md-3">
-            <h4>Contact Info</h4>
-            <p><i class="line-icon-map"></i>44 Main Street, New York, NY 25442</p>
-            <p><i class="line-icon-screen-smartphone"></i>+2-777-555-332 / -2-666-442-887</p>
-            <p><i class="line-icon-envelope-open"></i>support@themes-lab.com</p>
-            <p><i class="line-icon-calendar"></i>9am - 6 pm monday / saturday</p>
-          </div>
-          <div class="col-md-3">
-            <h4>Support</h4>
-            <p><a href="#">Faq</a></p>
-            <p><a href="#">Terms of Services</a></p>
-            <p><a href="#">Privacy Policy</a></p>
-            <p><a href="#">Contact us</a></p>
-          </div>
-          <div class="col-md-3">
-            <h4>Get In Touch</h4>
-            <div class="social-icons social-square">
-              <ul class="text-left">
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="300"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="500"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="700"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="900"><i class="fa fa-pinterest"></i></a></li>
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="1000"><i class="fa fa-flickr"></i></a></li>
-                <li><a href="#" class="animated" data-animation="fadeIn" data-animation-delay="1200"><i class="fa fa-linkedin"></i></a></li>
-              </ul>
+      <!-- </div> -->
+      <div class="section section-copyright bg-dark" >
+        <div class="full-width">
+          <div class="row">
+            <div class="col-md-6">
+              <p>Ahsan Gondal &copy; 2015</p>
+            </div>
+            <div class="col-md-6">
+              <p class="copyright-simple-menu">
+                <span>Home</span>
+                <span>Services</span>
+                <span>Pricing</span>
+                <span>Team</span>
+                <span>Support</span>
+              </p>
             </div>
           </div>
         </div>
-      </div>
     </div>
+      <!-- <div class="row"> -->
+      
+          
+        <!-- </div> -->
+
+    
+
+    <!-- Begin Footer 3 columns Dark -->
+    
     <!-- End Footer 3 columns Dark -->
 
     <!-- Begin Copyright Dark -->
-    <div class="section-copyright bg-dark">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <p>Themes Lab All rights reserved &copy; 2015</p>
-          </div>
-          <div class="col-md-6">
-            <p class="copyright-simple-menu">
-              <span>Home</span>
-              <span>Services</span>
-              <span>Pricing</span>
-              <span>Team</span>
-              <span>Support</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    
     <!-- End Copyright Dark -->
     
     <script src="<?php echo includes_dir;?>common-files/plugins/jquery/jquery-1.11.1.min.js"></script>
@@ -602,18 +517,12 @@
 
     <script>
         jQuery(document).ready(function($) {
-            var biggestHeight = "0";
+            $('.parallax').css("background-position", "50% 700px")
             // Loop through elements children to find & set the biggest height
-            $("#slider *").each(function(){
-             // If this elements height is bigger than the biggestHeight
-             if ($(this).height() > biggestHeight ) {
-               // Set the biggestHeight to this Height
-               biggestHeight = $(this).height();
-             }
-            });
+            
 
             // Set the container height
-            $("#slider").height(biggestHeight);
+            // alert($("#slider").height());
 
         });
     </script>
